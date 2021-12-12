f07dcc340f061a9c4118a4e1678537404d9da147
M0zzi3 <notmozzi3>
2021-12-12 16:40:57 +0100

    upadte3

--- RobotBrain/main.py
+++ RobotBrain/main.py
@@ -9,18 +9,23 @@ while True:
     try:
         client_input = server.listen()
 
-        print("Revice : " + client_input)
+        print("Revice : " + client_inpuadt)
 
-        if client_input == "END":
-            server.end_connection()
-
-        elif client_input == "Man":
+        if client_input == "Man":
             while True:
                 rob_command = server.listen()
+                print("Robot command : "+rob_command)
 
-                if rob_command != "end":
-                    rob.execute(rob_command)
-                else:
+                if rob_command == "end":
                     break
+                elif rob_command == "color" or rob_command == "distance":
+                    print(rob.manual_commands.get(rob_command)())
+                else:
+                    try:
+                        rob.manual_commands.get(rob_command)()
+                    except:
+                        pass
+
+
     except ConnectionResetError:
         server.accept_client()
--- RobotControl/control.py
+++ RobotControl/control.py
@@ -3,50 +3,60 @@ from pynput import keyboard
 
 user = client()
 
+
+def manual_mode():
+    print("Manual mode")
+
+    def on_press(key):
+
+        if key == keyboard.Key.esc:
+            print('')
+            user.send("end")
+            return False
+        else:
+            while True:
+                try:
+                    keyc = key.char
+                    break
+                except:
+                    pass
+
+            if keyc == 'w':
+                user.send("forward")
+            if keyc == 'a':
+                user.send("left")
+            if keyc == 's':
+                user.send("backward")
+            if keyc == 'd':
+                user.send("right")
+            if keyc == 'c':
+                user.send("color")
+            if keyc == 'x':
+                user.send("distance")
+            if keyc == 'r':
+                user.send('gear_up')
+            if keyc == 'f':
+                user.send('gear_down')
+
+    def on_release(key):
+        user.send("stop")
+
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
+
+user_commands = {
+    "Man": manual_mode
+}
+
 while True:
     msg = input("Message : ")
     user.send(msg)
 
-    if msg == "Man":
-        print("Manual mode")
+    try:
+        user_commands.get(msg)()
 
-
-        def on_press(key):
-
-            if key == keyboard.Key.esc:
-                return False
-            else:
-                while True:
-                    try:
-                        keyc = key.char
-                        break
-                    except:
-                        pass
-
-                if keyc == 'w':
-                    user.send("W")
-                if keyc == 'a':
-                    user.send("A")
-                if keyc == 's':
-                    user.send("S")
-                if keyc == 'd':
-                    user.send("D")
-                if keyc == 'c':
-                    user.send("Color")
-                if keyc == 'x':
-                    user.send("Distance")
-
-
-        def on_release(key):
-            user.send("stop")
-
-
-        with keyboard.Listener(
-                on_press=on_press,
-                on_release=on_release) as listener:
-            listener.join()
-
-        user.send("end")
-
-    elif msg == "Follow":
-        print("Now robot will be following the black line")
+    except:
+        pass
